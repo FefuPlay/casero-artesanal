f72208cae3da94f3d4fe3a3f139f635f40f6a0a7
FefuPlay <69704063+FefuPlay@users.noreply.github.com>
2020-08-16 02:13:16 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.presentacion');
+ScrollReveal().reveal('.produccion', {delay: 500});
+ScrollReveal().reveal('.productos', {delay: 500});
+ScrollReveal().reveal('.producto-estrella', {delay: 500});
